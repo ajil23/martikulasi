bf5e7435ab85c9eb72b788b4e0d0c2361e127281
ajil23 <azrilpraya@gmail.com>
2022-08-14 15:01:46 +0800

    first

--- simpan-daftar.php
+++ simpan-daftar.php
@@ -11,7 +11,7 @@ $anggota                = $_POST['id_anggota'];
 $dosen                  = $_POST['id_dosen'];
 $perusahaan             = $_POST['perusahaan'];
 // query SQL untuk insert data
-$query="INSERT INTO mahasiswa SET id='$nim',tempat_KP='$tempat',alamat_KP='$alamat',tanggal_Mulai='$tanggal_mulai',tanggal_Selesai='$tanggal_selesai',proposal='$proposal',id_anggota='$anggota',id_dosen='$dosen',perusahaan='$perusahaan'";
+$query="INSERT INTO pendaftar_kp SET id='$nim',tempat_KP='$tempat',alamat_KP='$alamat',tanggal_Mulai='$tanggal_mulai',tanggal_Selesai='$tanggal_selesai',proposal='$proposal',id_anggota='$anggota',id_dosen='$dosen',perusahaan='$perusahaan'";
 mysqli_query($koneksi, $query);
 // mengalihkan ke halaman index.php
 header("location:dash-admin.php");
