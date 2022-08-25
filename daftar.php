00af60fad0e2a96420cdc52294e7cfa17b1b2b15
ajil23 <azrilpraya@gmail.com>
2022-08-25 18:00:59 +0800

    first

--- daftar.php
+++ daftar.php
@@ -65,11 +65,6 @@
     </nav>
 
     <section class="dashboard">
-        <div class="top">
-            <i></i>
-            <img src="asset/dosen.jpg" alt="">
-        </div>
-
         <div class="dash-content">
             <div class="overview">
                 <div class="title">
