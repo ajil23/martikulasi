9050e129b584fa983a8dc425210ea0e1b67603ca
ajil23 <azrilpraya@gmail.com>
2022-08-25 22:59:17 +0800

    first

--- daftar-ujian.php
+++ daftar-ujian.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <!--=== Coding by CodingLab | www.codinglabweb.com === -->
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="daftar-ujian.css">
     <link rel="icon" sizes="180x180" href="asset/android-chrome-192x192.png">
     <link rel="icon" sizes="32x32" href="asset/poliwangi 32x32.png">
     <link rel="icon" sizes="16x16" href="asset/poliwangi 16x16.png">
-     
-     
+
+
     <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Pendaftar Ujian KP</title> 
+    <title>Pendaftar Ujian KP</title>
 </head>
+
 <body>
+
     <nav>
         <div class="logo-name">
             <div class="logo-image">
@@ -32,27 +35,27 @@
         <div class="menu-items">
             <ul class="nav-links">
                 <li><a href="dash-admin.php">
-                    <i class="uil uil-estate"></i>
-                    <span class="link-name">Dahsboard</span>
-                </a></li>
+                        <i class="uil uil-estate"></i>
+                        <span class="link-name">Dahsboard</span>
+                    </a></li>
                 <li><a href="daftar.php">
-                    <i class="uil uil-file-edit-alt"></i>
-                    <span class="link-name">Daftar</span>
-                </a></li>
+                        <i class="uil uil-file-edit-alt"></i>
+                        <span class="link-name">Daftar</span>
+                    </a></li>
                 <li><a href="#">
-                    <i class="uil uil-clipboard-notes"></i>
-                    <span class="link-name">Daftar Ujian</span>
-                </a></li>
+                        <i class="uil uil-clipboard-notes"></i>
+                        <span class="link-name">Daftar Ujian</span>
+                    </a></li>
             </ul>
-            
+
             <ul class="logout-mode">
                 <li><a href="index.php">
-                    <i class="uil uil-sign-out-alt"></i>
-                    <span class="link-name">Logout</span>
+                        <i class="uil uil-sign-out-alt"></i>
+                        <span class="link-name">Logout</span>
                     </a>
-                 </li>
-                 <li class="mode"> </li>
-                 <div class="mode-toggle"></div>
+                </li>
+                <li class="mode"> </li>
+                <div class="mode-toggle"></div>
                 </li>
             </ul>
         </div>
@@ -60,40 +63,50 @@
 
     <section class="dashboard">
         <div class="dash-content">
+            <div class="title">
+                <i class="uil uil-clipboard-notes"></i>
+                <span class="text">Pendaftar Ujian Kerja Praktek</span>
+            </div>
             <div class="overview">
-                <div class="title">
-                    <i class="uil uil-clipboard-notes"></i>
-                    <span class="text">Pendaftar Ujian Kerja Praktek</span>
-                </div>
                 <table class="table" border="1">
                     <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Pendaftar</th>
-                        <th scope="col">Laporan</th>
-                        <th scope="col">Jadwal</th>
-                    </tr>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Pendaftar</th>
+                            <th scope="col">Laporan</th>
+                            <th scope="col">Jadwal</th>
+                            <th scope="col" colspan="2">Aksi</th>
+                        </tr>
                     </thead>
-                    
+
                     <!--script php -->
                     <?php
                     include "koneksi.php";
-                    $ambildata = mysqli_query($koneksi,"select tb_pendaftaran_ujian_kp.Id_ujian,tb_pendaftaran_ujian_kp.Laporan_KP,tb_pendaftaran_ujian_kp.Jadwal_Ujian,tb_pendaftaran_kp.Id_pdftr from tb_pendaftaran_ujian_kp join tb_pendaftaran_kp on tb_pendaftaran_ujian_kp.Id_pdftr = tb_pendaftaran_kp.Id_pdftr ");
-                    while ($tampil = mysqli_fetch_array($ambildata)){
+                    $ambildata = mysqli_query($koneksi, "select tb_pendaftaran_ujian_kp.Id_ujian,tb_pendaftaran_ujian_kp.Laporan_KP,tb_pendaftaran_ujian_kp.Jadwal_Ujian,tb_pendaftaran_kp.Id_pdftr from tb_pendaftaran_ujian_kp join tb_pendaftaran_kp on tb_pendaftaran_ujian_kp.Id_pdftr = tb_pendaftaran_kp.Id_pdftr ");
+                    while ($tampil = mysqli_fetch_array($ambildata)) {
                         echo "
                         <tr>
                             <td>$tampil[Id_ujian]</td>
                             <td>$tampil[Id_pdftr]</td>
                             <td>$tampil[Laporan_KP]</td>
                             <td>$tampil[Jadwal_Ujian]</td>
-                        </tr>";
+                            <td>
+                            <a href='#' class='btn btn-warning'>Ubah</a>
+                            </td>
+                            <td>
+                            <a href='#' class='btn btn-danger'>Hapus</a>
+                            </td>
+                       </tr>";
                     }
                     ?>
                 </table>
+                <div class="float">
+                    <a href='#' class='btn btn-success'>Tambah data</a>
+                </div>
             </div>
         </div>
     </section>
-
     <script src="script.js"></script>
 </body>
+
 </html>
