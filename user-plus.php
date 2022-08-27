e398a895acb36021224f2eb636e18665e3bc2247
ajil23 <azrilpraya@gmail.com>
2022-08-27 11:11:29 +0800

    first

--- user-plus.php
+++ user-plus.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <!--=== Coding by CodingLab | www.codinglabweb.com === -->
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="dosenstyle.css">
     <link rel="icon" sizes="180x180" href="asset/android-chrome-192x192.png">
     <link rel="icon" sizes="32x32" href="asset/poliwangi 32x32.png">
     <link rel="icon" sizes="16x16" href="asset/poliwangi 16x16.png">
-     
-     
+
+
     <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>User</title> 
+    <title>User</title>
 </head>
+
 <body>
     <nav>
         <div class="logo-name">
@@ -31,39 +33,39 @@
         </div>
 
         <div class="menu-items">
-        <ul class="nav-links">
+            <ul class="nav-links">
                 <li><a href="admin.php">
-                    <i class="uil uil-file-graph"></i>
-                    <span class="link-name">Nilai Mahasiswa</span>
-                </a></li>
+                        <i class="uil uil-file-graph"></i>
+                        <span class="link-name">Nilai Mahasiswa</span>
+                    </a></li>
                 <li><a href="mahasiswa.php">
-                    <i class="uil uil-book-reader"></i>
-                    <span class="link-name">Mahasiswa</span>
-                </a></li>
+                        <i class="uil uil-book-reader"></i>
+                        <span class="link-name">Mahasiswa</span>
+                    </a></li>
                 <li><a href="dosen.php">
-                    <i class="uil uil-user-md"></i>
-                    <span class="link-name">Dosen</span>
-                </a></li>
+                        <i class="uil uil-user-md"></i>
+                        <span class="link-name">Dosen</span>
+                    </a></li>
                 <li><a href="user-plus.php">
-                    <i class="uil uil-user-plus"></i>
-                    <span class="link-name">User</span>
-                </a></li>
+                        <i class="uil uil-user-plus"></i>
+                        <span class="link-name">User</span>
+                    </a></li>
             </ul>
-            
+
             <ul class="logout-mode">
                 <li><a href="index.php">
-                    <i class="uil uil-sign-out-alt"></i>
-                    <span class="link-name">Logout</span>
-                </a></li>
+                        <i class="uil uil-sign-out-alt"></i>
+                        <span class="link-name">Logout</span>
+                    </a></li>
 
                 <li class="mode">
-                  
-                </a>
 
-                <div class="mode-toggle">
-                  
-                </div>
-            </li>
+                    </a>
+
+                    <div class="mode-toggle">
+
+                    </div>
+                </li>
             </ul>
         </div>
     </nav>
@@ -80,59 +82,53 @@
                     <i class="uil uil-user-plus"></i>
                     <span class="text">User</span>
                 </div>
-                
+
 
                 <table class="table" border="1">
                     <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Pemilik</th>
-                    </tr>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Pemilik</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
                     </thead>
-                    
+
                     <!--script php -->
-                    
+
                 </table>
             </div>
             <div class="float">
-                    <a href='#' class='btn btn-success' data-toggle="modal" data-target="#exampleModalLong">Tambah data</a>
-                </div>
-                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Masukkan Data</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form>
+                <a href='#' class='btn btn-success' data-toggle="modal" data-target="#exampleModalLong">Tambah data</a>
+            </div>
+            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Masukkan Data</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
                                 <div class="form-group">
-                                        <label for="formGroupExampleInput2">Judul Laporan Kerja Praktek</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Masukan Judul">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="formGroupExampleInput">File Laporan Kerja Praktek</label>
-                                        <div class="custom-file">
-                                            <input type="file" class="custom-file-input" id="customFile">
-                                            <label class="custom-file-label" for="customFile">Cari file</label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="formGroupExampleInput2">Jadwal</label>
-                                        <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-primary">Simpan</button>
-                            </div>
+                                    <label for="formGroupExampleInput2">Judul Laporan Kerja Praktek</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Masukan Judul">
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput2">Judul Laporan Kerja Praktek</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Masukan Judul">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Simpan</button>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     </section>
     <script src="script.js"></script>
@@ -140,4 +136,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
+
 </html>
