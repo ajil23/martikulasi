63cf65c17ab9afb20dfef75b82d172a7fc8b2b1f
Arilex <110915525+Xaisar@users.noreply.github.com>
2022-08-27 09:32:23 +0700

    database

--- db_martikulasi.sql
+++ db_martikulasi.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Agu 2022 pada 17.36
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 7.4.29
+-- Generation Time: Aug 27, 2022 at 04:29 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,40 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_acc_ujian`
+-- Table structure for table `tb_acc_ujian`
 --
 
 CREATE TABLE `tb_acc_ujian` (
-  `Id_acc` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Dosen_Penguji` varchar(45) DEFAULT NULL,
   `Jadwal_Ujian` datetime DEFAULT NULL,
   `ACC_Ujian` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_acc_ujian`
+-- Dumping data for table `tb_acc_ujian`
 --
 
-INSERT INTO `tb_acc_ujian` (`Id_acc`, `Dosen_Penguji`, `Jadwal_Ujian`, `ACC_Ujian`) VALUES
+INSERT INTO `tb_acc_ujian` (`Id`, `Dosen_Penguji`, `Jadwal_Ujian`, `ACC_Ujian`) VALUES
 (2526, 'Dianni Yusuf, S.Kom, M.Kom', '2022-08-29 00:18:58', 'Approve');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_anggota_kelompok`
+-- Table structure for table `tb_anggota_kelompok`
 --
 
 CREATE TABLE `tb_anggota_kelompok` (
-  `Id_angta` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Nama_Anggota` varchar(45) DEFAULT NULL,
   `NIM` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_anggota_kelompok`
+-- Dumping data for table `tb_anggota_kelompok`
 --
 
-INSERT INTO `tb_anggota_kelompok` (`Id_angta`, `Nama_Anggota`, `NIM`) VALUES
+INSERT INTO `tb_anggota_kelompok` (`Id`, `Nama_Anggota`, `NIM`) VALUES
 (95, 'Aisyah Almas Nur Salasabila', '362155401095'),
 (96, 'Nada Celia Sinka Audy Ines', '362155401096'),
 (97, 'Fauziah Putri Ramadhani', '362155401097'),
@@ -92,21 +92,21 @@ INSERT INTO `tb_anggota_kelompok` (`Id_angta`, `Nama_Anggota`, `NIM`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_dosen`
+-- Table structure for table `tb_dosen`
 --
 
 CREATE TABLE `tb_dosen` (
-  `Id_dsn` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Nama_Dosen` varchar(45) DEFAULT NULL,
   `NIK` varchar(45) DEFAULT NULL,
   `User_Id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_dosen`
+-- Dumping data for table `tb_dosen`
 --
 
-INSERT INTO `tb_dosen` (`Id_dsn`, `Nama_Dosen`, `NIK`, `User_Id`) VALUES
+INSERT INTO `tb_dosen` (`Id`, `Nama_Dosen`, `NIK`, `User_Id`) VALUES
 (197506, 'Endi Sailul Haq, S.T., M.Kom.', '197506252021211003', 3),
 (197601, 'Mohamad Dimyati Ayatullah, S.T., M.Kom.', '197601222021211001', 3),
 (197806, 'Muh. Fuad Al Haris, S.T., M.T.', '197806132014041001', 3),
@@ -126,45 +126,45 @@ INSERT INTO `tb_dosen` (`Id_dsn`, `Nama_Dosen`, `NIK`, `User_Id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_lembar_kerja`
+-- Table structure for table `tb_lembar_kerja`
 --
 
 CREATE TABLE `tb_lembar_kerja` (
-  `Id_lk` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Tanggal` datetime DEFAULT NULL,
   `File` varchar(45) DEFAULT NULL,
-  `Id_angta` int(11) DEFAULT NULL
+  `Anggota_Kelompok_Id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_lembar_kerja`
+-- Dumping data for table `tb_lembar_kerja`
 --
 
-INSERT INTO `tb_lembar_kerja` (`Id_lk`, `Tanggal`, `File`, `Id_angta`) VALUES
+INSERT INTO `tb_lembar_kerja` (`Id`, `Tanggal`, `File`, `Anggota_Kelompok_Id`) VALUES
 (9267, '2022-08-23 16:48:21', 'Proposal', 107);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_mahasiswa`
+-- Table structure for table `tb_mahasiswa`
 --
 
 CREATE TABLE `tb_mahasiswa` (
-  `Id_mhsa` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Nama_Mahasiswa` varchar(45) DEFAULT NULL,
   `NIM` varchar(45) DEFAULT NULL,
   `Kelas` varchar(45) DEFAULT NULL,
   `Email` varchar(45) DEFAULT NULL,
   `Alamat` varchar(45) DEFAULT NULL,
   `User_Id` int(11) DEFAULT NULL,
-  `Id_angta` int(11) DEFAULT NULL
+  `Anggota_Kelompok_Id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_mahasiswa`
+-- Dumping data for table `tb_mahasiswa`
 --
 
-INSERT INTO `tb_mahasiswa` (`Id_mhsa`, `Nama_Mahasiswa`, `NIM`, `Kelas`, `Email`, `Alamat`, `User_Id`, `Id_angta`) VALUES
+INSERT INTO `tb_mahasiswa` (`Id`, `Nama_Mahasiswa`, `NIM`, `Kelas`, `Email`, `Alamat`, `User_Id`, `Anggota_Kelompok_Id`) VALUES
 (1095, 'Aisyah Almas Nur Salsabila', '362155401095', 'D', 'salsabila@gmail.com', 'Kec. Kalipuro', 4, NULL),
 (1096, 'Nada Celia Sinka Audy Ines', '362155401096', 'D', 'nadacelia@gmail.com', 'Kembiritan', 4, NULL),
 (1097, 'Fauziah Putri Ramadhani', '362155401097', 'D', 'putriramadhani@gmail.com', 'Kec. Glenmore', 4, NULL),
@@ -199,49 +199,49 @@ INSERT INTO `tb_mahasiswa` (`Id_mhsa`, `Nama_Mahasiswa`, `NIM`, `Kelas`, `Email`
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_nilai`
+-- Table structure for table `tb_nilai`
 --
 
 CREATE TABLE `tb_nilai` (
-  `Id_nilai` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Nilai_Pembimbing_Lapangan` varchar(45) DEFAULT NULL,
   `Nilai_Pembimbing_KP` varchar(45) DEFAULT NULL,
   `Nilai_Penguji` varchar(45) DEFAULT NULL,
   `Bukti_Nilai_Pembimbing_Lapangan` varchar(45) DEFAULT NULL,
-  `Id_ujian` int(11) DEFAULT NULL
+  `Pendaftaran_Ujian_KP_Id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_nilai`
+-- Dumping data for table `tb_nilai`
 --
 
-INSERT INTO `tb_nilai` (`Id_nilai`, `Nilai_Pembimbing_Lapangan`, `Nilai_Pembimbing_KP`, `Nilai_Penguji`, `Bukti_Nilai_Pembimbing_Lapangan`, `Id_ujian`) VALUES
+INSERT INTO `tb_nilai` (`Id`, `Nilai_Pembimbing_Lapangan`, `Nilai_Pembimbing_KP`, `Nilai_Penguji`, `Bukti_Nilai_Pembimbing_Lapangan`, `Pendaftaran_Ujian_KP_Id`) VALUES
 (2473, '80', '80', '85', 'yes', 2512),
 (6234, '80', '85', '90', 'yes', 2412);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_pendaftaran_kp`
+-- Table structure for table `tb_pendaftaran_kp`
 --
 
 CREATE TABLE `tb_pendaftaran_kp` (
-  `Id_pdftr` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Tempat_KP` varchar(45) DEFAULT NULL,
   `Alamat_KP` varchar(45) DEFAULT NULL,
   `Tanggal_Mulai` datetime DEFAULT NULL,
   `Tanggal_Selesai` datetime DEFAULT NULL,
   `Proposal` varchar(45) DEFAULT NULL,
-  `Id_angta` int(11) DEFAULT NULL,
-  `Id_dsn` int(11) DEFAULT NULL,
-  `Id_corp` int(11) DEFAULT NULL
+  `Anggota_Kelompok_Id` int(11) DEFAULT NULL,
+  `Dosen_Id` int(11) DEFAULT NULL,
+  `Perusahaan_Id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_pendaftaran_kp`
+-- Dumping data for table `tb_pendaftaran_kp`
 --
 
-INSERT INTO `tb_pendaftaran_kp` (`Id_pdftr`, `Tempat_KP`, `Alamat_KP`, `Tanggal_Mulai`, `Tanggal_Selesai`, `Proposal`, `Id_angta`, `Id_dsn`, `Id_corp`) VALUES
+INSERT INTO `tb_pendaftaran_kp` (`Id`, `Tempat_KP`, `Alamat_KP`, `Tanggal_Mulai`, `Tanggal_Selesai`, `Proposal`, `Anggota_Kelompok_Id`, `Dosen_Id`, `Perusahaan_Id`) VALUES
 (164, 'PT. Tokopedia', 'Jakarta Pusat', '2022-08-23 22:28:39', '2022-09-23 22:28:39', 'Pelopor Perusahaan Unicorn di Indonesia', 107, 198403, 1436),
 (165, 'PT. Tokopedia', 'Jakarta Pusat', '2022-08-23 00:05:53', '2022-09-23 00:05:53', 'Pelopor Perusahaan Unicorn di Indonesia', 114, 198403, 1436),
 (909, 'PT. Bank Jago Tbk', 'Jakarta Pusat', '2022-08-24 00:05:53', '2022-08-31 20:15:00', 'Perusahaan Unicorn di Indonesia', 124, 197806, 5556),
@@ -250,33 +250,33 @@ INSERT INTO `tb_pendaftaran_kp` (`Id_pdftr`, `Tempat_KP`, `Alamat_KP`, `Tanggal_
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_pendaftaran_ujian_kp`
+-- Table structure for table `tb_pendaftaran_ujian_kp`
 --
 
 CREATE TABLE `tb_pendaftaran_ujian_kp` (
-  `Id_ujian` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Laporan_KP` varchar(45) DEFAULT NULL,
   `Jadwal_Ujian` varchar(45) DEFAULT NULL,
-  `Id_pdftr` int(11) DEFAULT NULL,
-  `Id_acc` int(11) DEFAULT NULL
+  `Pendaftaran_KP_Id` int(11) DEFAULT NULL,
+  `ACC_Ujian_Id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_pendaftaran_ujian_kp`
+-- Dumping data for table `tb_pendaftaran_ujian_kp`
 --
 
-INSERT INTO `tb_pendaftaran_ujian_kp` (`Id_ujian`, `Laporan_KP`, `Jadwal_Ujian`, `Id_pdftr`, `Id_acc`) VALUES
+INSERT INTO `tb_pendaftaran_ujian_kp` (`Id`, `Laporan_KP`, `Jadwal_Ujian`, `Pendaftaran_KP_Id`, `ACC_Ujian_Id`) VALUES
 (2412, 'Pelopor Perusahaan Unicorn di Indonesia', '23 Agustus - 23 September 2022', 165, 2526),
 (2512, 'Pelopor Perusahaan Unicorn di Indonesia', '23 Agustus - 23 September 2022', 164, 2526);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_perusahaan`
+-- Table structure for table `tb_perusahaan`
 --
 
 CREATE TABLE `tb_perusahaan` (
-  `Id_corp` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Nama_Perusahaan` varchar(45) DEFAULT NULL,
   `Alamat` varchar(45) DEFAULT NULL,
   `Email` varchar(45) DEFAULT NULL,
@@ -284,10 +284,10 @@ CREATE TABLE `tb_perusahaan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_perusahaan`
+-- Dumping data for table `tb_perusahaan`
 --
 
-INSERT INTO `tb_perusahaan` (`Id_corp`, `Nama_Perusahaan`, `Alamat`, `Email`, `Telephone`) VALUES
+INSERT INTO `tb_perusahaan` (`Id`, `Nama_Perusahaan`, `Alamat`, `Email`, `Telephone`) VALUES
 (1216, 'PT. GoTo Gojek Tokopedia TBK', 'Jakarta Selatan', 'info@go-jek.com', '0-21-690-5784'),
 (1271, 'PT. Espay Debit Indonesia Koe', 'Jakarta Pusat', 'help@dana.id', '0-21-452-9685'),
 (1436, 'PT. Tokopedia ', 'Jakarta Pusat', 'android.feedback@tokopedia.com', '0-21-831-0155 '),
@@ -298,21 +298,21 @@ INSERT INTO `tb_perusahaan` (`Id_corp`, `Nama_Perusahaan`, `Alamat`, `Email`, `T
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_user`
+-- Table structure for table `tb_user`
 --
 
 CREATE TABLE `tb_user` (
-  `User_Id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Username` varchar(45) DEFAULT NULL,
   `Password` varchar(45) DEFAULT NULL,
   `Id_Role` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_user`
+-- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`User_Id`, `Username`, `Password`, `Id_Role`) VALUES
+INSERT INTO `tb_user` (`Id`, `Username`, `Password`, `Id_Role`) VALUES
 (1, 'koordinator', '12345', 3),
 (2, 'admin', '12345', 4),
 (3, 'dosen', '12345', 2),
@@ -321,19 +321,19 @@ INSERT INTO `tb_user` (`User_Id`, `Username`, `Password`, `Id_Role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_user_role`
+-- Table structure for table `tb_user_role`
 --
 
 CREATE TABLE `tb_user_role` (
-  `Id_Role` int(11) NOT NULL,
+  `Id_User` int(11) NOT NULL,
   `Role` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tb_user_role`
+-- Dumping data for table `tb_user_role`
 --
 
-INSERT INTO `tb_user_role` (`Id_Role`, `Role`) VALUES
+INSERT INTO `tb_user_role` (`Id_User`, `Role`) VALUES
 (1, 'Mahasiswa'),
 (2, 'Dosen'),
 (3, 'Koordinator KP'),
@@ -344,142 +344,142 @@ INSERT INTO `tb_user_role` (`Id_Role`, `Role`) VALUES
 --
 
 --
--- Indeks untuk tabel `tb_acc_ujian`
+-- Indexes for table `tb_acc_ujian`
 --
 ALTER TABLE `tb_acc_ujian`
-  ADD PRIMARY KEY (`Id_acc`),
-  ADD UNIQUE KEY `idx_acc_ujian` (`Id_acc`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_acc_ujian` (`Id`);
 
 --
--- Indeks untuk tabel `tb_anggota_kelompok`
+-- Indexes for table `tb_anggota_kelompok`
 --
 ALTER TABLE `tb_anggota_kelompok`
-  ADD PRIMARY KEY (`Id_angta`),
-  ADD UNIQUE KEY `idx_anggota_kelompok` (`Id_angta`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_anggota_kelompok` (`Id`);
 
 --
--- Indeks untuk tabel `tb_dosen`
+-- Indexes for table `tb_dosen`
 --
 ALTER TABLE `tb_dosen`
-  ADD PRIMARY KEY (`Id_dsn`),
-  ADD UNIQUE KEY `idx_dosen` (`Id_dsn`),
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_dosen` (`Id`),
   ADD KEY `User_Id` (`User_Id`);
 
 --
--- Indeks untuk tabel `tb_lembar_kerja`
+-- Indexes for table `tb_lembar_kerja`
 --
 ALTER TABLE `tb_lembar_kerja`
-  ADD PRIMARY KEY (`Id_lk`),
-  ADD UNIQUE KEY `idx_lembar_kerja` (`Id_lk`),
-  ADD KEY `Anggota_Kelompok_Id` (`Id_angta`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_lembar_kerja` (`Id`),
+  ADD KEY `Anggota_Kelompok_Id` (`Anggota_Kelompok_Id`);
 
 --
--- Indeks untuk tabel `tb_mahasiswa`
+-- Indexes for table `tb_mahasiswa`
 --
 ALTER TABLE `tb_mahasiswa`
-  ADD PRIMARY KEY (`Id_mhsa`),
-  ADD UNIQUE KEY `idx_mahasiswa` (`Id_mhsa`),
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_mahasiswa` (`Id`),
   ADD KEY `User_Id` (`User_Id`),
-  ADD KEY `Anggota_Kelompok_Id` (`Id_angta`);
+  ADD KEY `Anggota_Kelompok_Id` (`Anggota_Kelompok_Id`);
 
 --
--- Indeks untuk tabel `tb_nilai`
+-- Indexes for table `tb_nilai`
 --
 ALTER TABLE `tb_nilai`
-  ADD PRIMARY KEY (`Id_nilai`),
-  ADD UNIQUE KEY `idx_nilai` (`Id_nilai`),
-  ADD KEY `Pendaftaran_Ujian_KP_Id` (`Id_ujian`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_nilai` (`Id`),
+  ADD KEY `Pendaftaran_Ujian_KP_Id` (`Pendaftaran_Ujian_KP_Id`);
 
 --
--- Indeks untuk tabel `tb_pendaftaran_kp`
+-- Indexes for table `tb_pendaftaran_kp`
 --
 ALTER TABLE `tb_pendaftaran_kp`
-  ADD PRIMARY KEY (`Id_pdftr`),
-  ADD UNIQUE KEY `idx_pendaftaran_kp` (`Id_pdftr`),
-  ADD KEY `Anggota_Kelompok_Id` (`Id_angta`),
-  ADD KEY `Dosen_Id` (`Id_dsn`),
-  ADD KEY `Perusahaan_Id` (`Id_corp`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_pendaftaran_kp` (`Id`),
+  ADD KEY `Anggota_Kelompok_Id` (`Anggota_Kelompok_Id`),
+  ADD KEY `Dosen_Id` (`Dosen_Id`),
+  ADD KEY `Perusahaan_Id` (`Perusahaan_Id`);
 
 --
--- Indeks untuk tabel `tb_pendaftaran_ujian_kp`
+-- Indexes for table `tb_pendaftaran_ujian_kp`
 --
 ALTER TABLE `tb_pendaftaran_ujian_kp`
-  ADD PRIMARY KEY (`Id_ujian`),
-  ADD UNIQUE KEY `idx_pendaftaran_ujian_kp` (`Id_ujian`),
-  ADD KEY `Pendaftaran_KP_Id` (`Id_pdftr`),
-  ADD KEY `ACC_Ujian_Id` (`Id_acc`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_pendaftaran_ujian_kp` (`Id`),
+  ADD KEY `Pendaftaran_KP_Id` (`Pendaftaran_KP_Id`),
+  ADD KEY `ACC_Ujian_Id` (`ACC_Ujian_Id`);
 
 --
--- Indeks untuk tabel `tb_perusahaan`
+-- Indexes for table `tb_perusahaan`
 --
 ALTER TABLE `tb_perusahaan`
-  ADD PRIMARY KEY (`Id_corp`),
-  ADD UNIQUE KEY `idx_perusahaan` (`Id_corp`);
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_perusahaan` (`Id`);
 
 --
--- Indeks untuk tabel `tb_user`
+-- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`User_Id`),
-  ADD UNIQUE KEY `idx_user` (`User_Id`),
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `idx_user` (`Id`),
   ADD KEY `Id_Role` (`Id_Role`);
 
 --
--- Indeks untuk tabel `tb_user_role`
+-- Indexes for table `tb_user_role`
 --
 ALTER TABLE `tb_user_role`
-  ADD PRIMARY KEY (`Id_Role`),
-  ADD UNIQUE KEY `idx_user_role` (`Id_Role`);
+  ADD PRIMARY KEY (`Id_User`),
+  ADD UNIQUE KEY `idx_user_role` (`Id_User`);
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `tb_dosen`
+-- Constraints for table `tb_dosen`
 --
 ALTER TABLE `tb_dosen`
-  ADD CONSTRAINT `tb_dosen_ibfk_1` FOREIGN KEY (`User_Id`) REFERENCES `tb_user` (`User_Id`);
+  ADD CONSTRAINT `tb_dosen_ibfk_1` FOREIGN KEY (`User_Id`) REFERENCES `tb_user` (`Id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_lembar_kerja`
+-- Constraints for table `tb_lembar_kerja`
 --
 ALTER TABLE `tb_lembar_kerja`
-  ADD CONSTRAINT `tb_lembar_kerja_ibfk_1` FOREIGN KEY (`Id_angta`) REFERENCES `tb_anggota_kelompok` (`Id_angta`);
+  ADD CONSTRAINT `tb_lembar_kerja_ibfk_1` FOREIGN KEY (`Anggota_Kelompok_Id`) REFERENCES `tb_anggota_kelompok` (`Id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_mahasiswa`
+-- Constraints for table `tb_mahasiswa`
 --
 ALTER TABLE `tb_mahasiswa`
-  ADD CONSTRAINT `tb_mahasiswa_ibfk_1` FOREIGN KEY (`User_Id`) REFERENCES `tb_user` (`User_Id`),
-  ADD CONSTRAINT `tb_mahasiswa_ibfk_2` FOREIGN KEY (`Id_angta`) REFERENCES `tb_anggota_kelompok` (`Id_angta`);
+  ADD CONSTRAINT `tb_mahasiswa_ibfk_1` FOREIGN KEY (`User_Id`) REFERENCES `tb_user` (`Id`),
+  ADD CONSTRAINT `tb_mahasiswa_ibfk_2` FOREIGN KEY (`Anggota_Kelompok_Id`) REFERENCES `tb_anggota_kelompok` (`Id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_nilai`
+-- Constraints for table `tb_nilai`
 --
 ALTER TABLE `tb_nilai`
-  ADD CONSTRAINT `tb_nilai_ibfk_1` FOREIGN KEY (`Id_ujian`) REFERENCES `tb_pendaftaran_ujian_kp` (`Id_ujian`);
+  ADD CONSTRAINT `tb_nilai_ibfk_1` FOREIGN KEY (`Pendaftaran_Ujian_KP_Id`) REFERENCES `tb_pendaftaran_ujian_kp` (`Id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_pendaftaran_kp`
+-- Constraints for table `tb_pendaftaran_kp`
 --
 ALTER TABLE `tb_pendaftaran_kp`
-  ADD CONSTRAINT `tb_pendaftaran_kp_ibfk_1` FOREIGN KEY (`Id_angta`) REFERENCES `tb_anggota_kelompok` (`Id_angta`),
-  ADD CONSTRAINT `tb_pendaftaran_kp_ibfk_2` FOREIGN KEY (`Id_dsn`) REFERENCES `tb_dosen` (`Id_dsn`),
-  ADD CONSTRAINT `tb_pendaftaran_kp_ibfk_3` FOREIGN KEY (`Id_corp`) REFERENCES `tb_perusahaan` (`Id_corp`);
+  ADD CONSTRAINT `tb_pendaftaran_kp_ibfk_1` FOREIGN KEY (`Anggota_Kelompok_Id`) REFERENCES `tb_anggota_kelompok` (`Id`),
+  ADD CONSTRAINT `tb_pendaftaran_kp_ibfk_2` FOREIGN KEY (`Dosen_Id`) REFERENCES `tb_dosen` (`Id`),
+  ADD CONSTRAINT `tb_pendaftaran_kp_ibfk_3` FOREIGN KEY (`Perusahaan_Id`) REFERENCES `tb_perusahaan` (`Id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_pendaftaran_ujian_kp`
+-- Constraints for table `tb_pendaftaran_ujian_kp`
 --
 ALTER TABLE `tb_pendaftaran_ujian_kp`
-  ADD CONSTRAINT `tb_pendaftaran_ujian_kp_ibfk_1` FOREIGN KEY (`Id_pdftr`) REFERENCES `tb_pendaftaran_kp` (`Id_pdftr`),
-  ADD CONSTRAINT `tb_pendaftaran_ujian_kp_ibfk_2` FOREIGN KEY (`Id_acc`) REFERENCES `tb_acc_ujian` (`Id_acc`);
+  ADD CONSTRAINT `tb_pendaftaran_ujian_kp_ibfk_1` FOREIGN KEY (`Pendaftaran_KP_Id`) REFERENCES `tb_pendaftaran_kp` (`Id`),
+  ADD CONSTRAINT `tb_pendaftaran_ujian_kp_ibfk_2` FOREIGN KEY (`ACC_Ujian_Id`) REFERENCES `tb_acc_ujian` (`Id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_user`
+-- Constraints for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`Id_Role`) REFERENCES `tb_user_role` (`Id_Role`);
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`Id_Role`) REFERENCES `tb_user_role` (`Id_User`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
